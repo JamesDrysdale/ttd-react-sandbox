61f7bdef02879355de2c16f94faf480df907ff03
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-03-16 10:59:57 +0000

    Code arranged into component and container folders to keep it organised as project grows

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 
-import logo from './logo.svg';
-import './App.css';
-
 function Header() {
   return (
     <div className="Header">
--- src/containers/__tests__/App.test.js
+++ src/containers/__tests__/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './Header';
+import App from '../App';
 
 test('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Header />, div);
+  ReactDOM.render(<App />, div);
 });
