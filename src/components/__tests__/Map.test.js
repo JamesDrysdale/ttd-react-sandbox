064c29e6cda2f159bd43b7fb6784de6ecd0406ad
JamesDrysdale <jamesdrysdale84@gmail.com.com>
2021-03-17 17:21:13 +0000

    Using props to make app dynamic

--- src/components/__tests__/Map.test.js
+++ src/components/__tests__/Map.test.js
@@ -4,9 +4,14 @@ import Map from '../Map';
 
 describe("Map", function(){
   let mountedMap;
+  let props;
 
   beforeEach(() => {
-    mountedMap = shallow(<Map />);
+    props = {
+      location: undefined,
+      imagename: 'testmap.png'
+    }
+    mountedMap = shallow(<Map {...props} />);
   });
 
   it('renders without crashing', () => {
@@ -19,9 +24,15 @@ describe("Map", function(){
   });
 
   it('displays the none map when no params are given', () => {
-    const defaultMap = mountedMap.find('img[src="images/none.png"]');
+    let defaultMap = shallow(<Map />);
+    const defaultImage = defaultMap.find('img[src="images/none.png"]');
     expect(defaultMap.length).toBe(1);
   });
+
+  it('displays the map imagename passed to it', () => {
+    const testMap = mountedMap.find('img[src="images/testmap.png"]');
+    expect(testMap.length).toBe(1);
+  })
 
 })
 
--- src/containers/StoreLocator.js
+++ src/containers/StoreLocator.js
@@ -4,14 +4,34 @@ import Button from '../components/Button';
 import Map from '../components/Map';
 
 class StoreLocator extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentMap: 'none.png'
+        }
+        this.shops = [{
+            'location' : 'Portland',
+            'address'  : '123 Portland Dr'
+        }, {
+            'location' : 'Astoria',
+            'address'  : '456 Astoria Pl'
+        }, {
+            'location' : '',
+            'address'  : ''
+        }];
+    }
     render() {
+        let storeButtons = this.shops.map((shop, id) => {
+            return(<Button key={id} location={shop.location} />)
+        })
+
         return (<Fragment>
                     <Header />
                     <div>
-                        <Button location="Portland" /> <Button location="Astoria" /> <Button />
+                        {storeButtons}
                     </div>
                     
-                    <Map />
+                    <Map imagename={this.state.currentMap} location={this.props.location} />
                 </Fragment>);
     }
 }
